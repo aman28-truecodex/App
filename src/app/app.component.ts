abce19d999e092f358024a5beb2afc9c69fe544f
TabassumBano <tabassum.shaikh315@gmail.com>
2020-10-15 20:24:57 +0530

    Revert "PI:2858: commit updated-3"
    This reverts commit 1db2b9a9c439c57a4a1a8cf1436fede3d0908779.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { iEmply } from 'src/assets/model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Datatable';
 
- 
+  dtOptions : DataTables.Settings = {};
   constructor(){}
- 
-  ngOnInit() {}
+  public array:iEmply[]=[
+  {id:'1', name:'aman', role:'admin'},
+  {id:'2', name:'vinay', role:'admin'},
+  {id:'3', name:'vimal', role:'admin'},
+  {id:'4', name:'Tab', role:'admin'},
+  {id:'5', name:'volume', role:'admin'},
+  
+
+]
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2,
+      processing: true
+    };
+}
 }
